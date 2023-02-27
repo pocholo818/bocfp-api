3453f4306f73ebdfbb5b30f8671f0b570d095a8a
pocholo818 <89579920+pocholo818@users.noreply.github.com>
2023-02-28 00:11:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,10 @@ const connection = mysql.createConnection({
   user:  'u621496327_bocfp',
   password: 'Bocfp2022$',
   database: 'u621496327_bocfp'
+  // host: 'localhost',
+  // user:  'root',
+  // password: '',
+  // database: 'bocfp'
 })
 
 function authenticateToken(admin_power) {
@@ -82,9 +86,8 @@ app.post('/user/login', (req, res) => {
   const { username, password } = req.body
 
   connection.query(`SELECT * FROM user WHERE username='${username}' AND password='${password}' AND soft_delete = 0`, (err, rows, fields) => {
-    console.log(rows)
-    if (!rows) {
-      res.json({ "message": "Incorrect Username or Password" })
+    if (!rows[0]) {
+      res.sendStatus(401).json({ "message": "Incorrect Username or Password" })
     }
     else if (rows[0].username == username && rows[0].password == password) {
 
