5230519bb78c9c0f6bbf80071140c99b2b37e476
pocholo818 <pochologopez2000@gmail.com>
2023-06-11 16:40:38 +0800

    changes
    -remove the 7 - 12 yrs old on age
    -added 13 and 14 to purok
    -added a new db with more data
    -backup db for old one

--- index.js
+++ index.js
@@ -969,12 +969,12 @@ app.get('/child/remarks', (req, res) => {
 // get all age
 app.get('/child/age', (req, res) => {
   let results = {
-    1: 0,
-    2: 0,
-    3: 0,
-    4: 0,
-    5: 0,
-    6: 0,
+    // 1: 0,
+    // 2: 0,
+    // 3: 0,
+    // 4: 0,
+    // 5: 0,
+    // 6: 0,
     7: 0,
     8: 0,
     9: 0,
@@ -1015,6 +1015,8 @@ app.get('/child/purok', (req, res) => {
     10: 0,
     11: 0,
     12: 0,
+    13: 0,
+    14: 0,
     "total": 0
   }
 
